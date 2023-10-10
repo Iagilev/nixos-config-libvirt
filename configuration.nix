bc93900df04b6a740bef3edb334d55949d1020dd
Ivan <63961408+Iagilev@users.noreply.github.com>
2023-10-10 21:52:49 +0400

    NixOS Flake simple configuration

---
+++ configuration.nix
@@ -0,0 +1,253 @@
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  networking.hostName = "nixbox";
+  networking.hostId = "8318cad8"; # head -c8 /etc/machine-id
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.utf8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.utf8";
+    LC_IDENTIFICATION = "en_US.utf8";
+    LC_MEASUREMENT = "en_US.utf8";
+    LC_MONETARY = "en_US.utf8";
+    LC_NAME = "en_US.utf8";
+    LC_NUMERIC = "en_US.utf8";
+    LC_PAPER = "en_US.utf8";
+    LC_TELEPHONE = "en_US.utf8";
+    LC_TIME = "en_US.utf8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Enable the QEMU Guest Agent
+  services.qemuGuest.enable = true;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.root = { password = "vagrant"; };
+  # Creates a "vagrant" group & user with password-less sudo access
+  users.groups.vagrant = {
+    name = "vagrant";
+    members = [ "vagrant" ];
+  };
+  users.users.vagrant = {
+    description = "Vagrant User";
+    name = "vagrant";
+    group = "vagrant";
+    extraGroups = [ "users" "wheel" "docker" "networkmanager" ];
+    password = "vagrant";
+    home = "/home/vagrant";
+    createHome = true;
+    useDefaultShell = true;
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
+    ];
+    packages = with pkgs; [
+      # here is some command line tools I use frequently
+      # feel free to add your own or remove some of them
+
+      neofetch
+      nnn # terminal file manager
+
+      # archives
+      zip
+      xz
+      unzip
+      p7zip
+
+      # utils
+      ripgrep # recursively searches directories for a regex pattern
+      jq # A lightweight and flexible command-line JSON processor
+      yq-go # yaml processer https://github.com/mikefarah/yq
+      exa # A modern replacement for ‘ls’
+      fzf # A command-line fuzzy finder
+      bat # A modern replacement for ‘cat’
+
+      # networking tools
+      mtr # A network diagnostic tool
+      iperf3
+      dnsutils  # `dig` + `nslookup`
+      ldns # replacement of `dig`, it provide the command `drill`
+      aria2 # A lightweight multi-protocol & multi-source command-line download utility
+      socat # replacement of openbsd-netcat
+      nmap # A utility for network discovery and security auditing
+      ipcalc  # it is a calculator for the IPv4/v6 addresses
+
+      # misc
+      cowsay
+      file
+      which
+      tree
+      gnused
+      gnutar
+      gawk
+      zstd
+      gnupg
+
+      # nix related
+      #
+      # it provides the command `nom` works just like `nix`
+      # with more details log output
+      nix-output-monitor
+
+      # productivity
+      hugo # static site generator
+      glow # markdown previewer in terminal
+
+      btop  # replacement of htop/nmon
+      iotop # io monitoring
+      iftop # network monitoring
+
+      # system call monitoring
+      strace # system call monitoring
+      ltrace # library call monitoring
+      lsof # list open files
+
+      # system tools
+      sysstat
+      lm_sensors # for `sensors` command
+      ethtool
+      pciutils # lspci
+      usbutils # lsusb
+    ];
+  };
+
+  security.sudo.extraConfig =
+    ''
+      Defaults:root,%wheel env_keep+=LOCALE_ARCHIVE
+      Defaults:root,%wheel env_keep+=NIX_PATH
+      Defaults:root,%wheel env_keep+=TERMINFO_DIRS
+      Defaults env_keep+=SSH_AUTH_SOCK
+      Defaults lecture = never
+      root   ALL=(ALL) SETENV: ALL
+      %wheel ALL=(ALL) NOPASSWD: ALL, SETENV: ALL
+    '';
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # Packages for Vagrant
+  environment.systemPackages = with pkgs; [
+    git
+    findutils
+    gnumake
+    iputils
+    jq
+    nettools
+    netcat
+    nfs-utils
+    rsync
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Enable DBus
+  services.dbus.enable = true;
+
+  # Replace ntpd by timesyncd
+  services.timesyncd.enable = true;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # system.stateVersion = "23.05";
+  system = {
+    autoUpgrade = {
+      enable = true;
+      channel = "https://nixos.org/channels/nixos-23.05";
+    };
+    stateVersion = "23.05";
+  };
+
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 2d";
+    };
+    extraOptions = "experimental-features = nix-command flakes";
+  };
+
+  # Optimize storage
+  # You can also manually optimize the store via:
+  #    nix-store --optimise
+  # Refer to the following link for more details:
+  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
+  nix.settings.auto-optimise-store = true;
+
+  # Use the GRUB 2 EFI boot loader.
+  boot.loader.systemd-boot = {
+    enable = false;
+    editor = false;
+  };
+  boot.loader.efi = {
+    canTouchEfiVariables = false;
+  };
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+    fsIdentifier = "label";
+    splashMode = "stretch";
+    device = "nodev";
+    extraEntries = ''
+      menuentry "Reboot" {
+        reboot
+      }
+      menuentry "Poweroff" {
+        halt
+      }
+    '';
+  };
+
+  # Kernel
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_1;
+
+  # Limit the number of generations to keep
+  boot.loader.grub.configurationLimit = 10;
+
+  # Configure ZFS
+  boot.supportedFilesystems = [ "zfs" "ntfs" ];
+
+  # remove the fsck that runs at startup. It will always fail to run, stopping
+  # your boot until you press *.
+  boot.initrd.checkJournalingFS = false;
+}
